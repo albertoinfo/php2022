3b489f1216ef1207ce566bf274dca729e2d34c5f
albertoinfo <alberto.lopez@iestetuan.es>
2022-11-10 22:55:21 +0100

    Ejemplos POO mejorados

---
+++ POO_ejemplos/herencia/CuentaBancariaConCredito.php
@@ -0,0 +1,36 @@
+<?php 
+class CuentaBancariaConCredito extends CuentaBancaria
+{
+    // -------------------------------------
+    // Atributos de Clase
+    // -------------------------------------
+    
+    private $interes;   // Interes a pagar con saldo negativo
+
+    // -------------------------------------
+    //   METODOS:
+    // -------------------------------------
+    
+    // Constructores
+    public function __construct(int $saldo = 0, $interes =0.10){
+        parent::__construct($saldo);
+        $this->interes = $interes;
+    }
+    
+    
+    // Abono, decremento el saldo en la cantidad indicada como parámetro.
+    // Sobreescribo la clase padre
+    public function abono (int $cantidad){
+        if ( ($this->saldo - $cantidad) > 0){
+            $this->saldo -= $cantidad;
+        } else {
+            // Se queda con una deuda con interes
+            $credito = $cantidad - $this->saldo;
+            $this->saldo = - ($credito + $credito*$this->interes);
+            echo "Crédito concedido \n";
+        }
+        $this->numMovimientos++;
+    }
+    
+}
+
